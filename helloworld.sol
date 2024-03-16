e2cb898c81476459c8f4d6e69e52574300d356d7
Rasesh <90860591+010100100100011101010100@users.noreply.github.com>
2024-03-16 14:06:06 +0530

    Add files via upload

---
+++ helloworld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "hardhat/console.sol";
+contract helloworld{
+    function sayhello() public pure returns (string memory){
+        console.log("hello world");
+    return "hello rasesh";}
+}
